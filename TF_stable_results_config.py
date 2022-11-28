d43f4c42eae0bc103ac00f4e00e7c988fb583617
Lingfeng(Rinn) Zhang <41410483+RichardChangCA@users.noreply.github.com>
2022-11-28 00:17:05 -0500

    Create TF_stable_results_config.py

---
+++ TF_stable_results_config.py
@@ -0,0 +1,24 @@
+SEED = 1234
+
+import os
+import random
+import numpy as np
+import tensorflow as tf
+
+def set_seeds(seed=SEED):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+def set_global_determinism(seed=SEED):
+    set_seeds(seed=seed)
+
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+    
+    tf.config.threading.set_inter_op_parallelism_threads(1)
+    tf.config.threading.set_intra_op_parallelism_threads(1)
+
+# Call the above function with seed value
+set_global_determinism(seed=SEED)
